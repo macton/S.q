6d0f05b9f9108a09dcc04a2bf8f0a0a936815ec1
Mike Acton <macton@gmail.com>
2015-07-13 17:19:24 -0700

    Q.longStackSupport = true;

--- s.q.js
+++ s.q.js
@@ -1,6 +1,8 @@
 var Q = require('q');
 var S = require('s');
 
+Q.longStackSupport = true;
+
 var q = {
   catch: function( fn ) { fn.catch = true; return fn; },
   done:  function( fn ) { fn.done = true; return fn; },
